701a3bcfd6a0b11ec204016238fefeca855b93a3
bIgMaT29 <mathieu.gomila2023@campus-eni.fr>
2023-09-27 15:43:20 +0200

    Maj Config

--- src/main/java/fr/eni/enchere/dal/jdbc/ConnectionProvider.java
+++ src/main/java/fr/eni/enchere/dal/jdbc/ConnectionProvider.java
@@ -19,7 +19,7 @@ abstract class ConnectionProvider {
 		Context context;
 		try {
 			context = new InitialContext();
-			ConnectionProvider.dataSource = (DataSource)context.lookup("java:comp/env/jdbc/pool_cnx_encheres_OVH");
+			ConnectionProvider.dataSource = (DataSource)context.lookup("java:comp/env/jdbc/pool_cnx_encheres");
 		} catch (NamingException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Impossible d'accéder à la base de données");
